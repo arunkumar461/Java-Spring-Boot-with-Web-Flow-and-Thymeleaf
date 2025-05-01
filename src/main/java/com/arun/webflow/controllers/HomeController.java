b50b9f684c2ebce39aded88273caafd068225c6d
Arun Kumar Madesh <56151567+arunkumar461@users.noreply.github.com>
2025-05-01 15:10:14 +0100

    update controller

--- src/main/java/com/arun/webflow/controllers/HomeController.java
+++ src/main/java/com/arun/webflow/controllers/HomeController.java
@@ -2,15 +2,14 @@ package com.arun.webflow.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping(method = RequestMethod.GET, value = "/")
-	public String index(Model model) {
-		model.addAttribute("message", "Hello, world!");
-		return "index";
-	}
+  @GetMapping(value = "/")
+  public String index(Model model) {
+    model.addAttribute("message", "Spring boot webflow example");
+    return "index";
+  }
 }
